6664dc2fb272fa3877c924cfcbeb2d828eddfde1
vaujan <fauzan20014@gmail.com>
2025-06-23 16:55:57 +0700

    wip: auth supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import "./App.css";
 // import Testing2 from "./components/timer/testing-2";
 // import Todo from "./components/todo/todo";
-import React from "react";
+import React, { useEffect } from "react";
 import Auth from "./components/auth/auth";
+import { supabase } from "./lib/supabase";
 
 function App() {
+	const [session, setSession] = React.useState(null);
+
+	const fetchSession = async () => {
+		const currentSession = await supabase.auth.getSession();
+		console.log("Current session:", currentSession);
+		setSession(currentSession.data);
+	};
+
+	React.useEffect(() => {
+		fetchSession();
+	}, [session]);
+
 	return (
 		<div className="flex gap-6">
 			{/* <Testing2 /> */}
--- src/components/auth/auth.tsx
+++ src/components/auth/auth.tsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, from "react";
+import { supabase } from "../../lib/supabase";
 
 type AuthState = "sign up" | "sign in";
 
 export default function Auth() {
 	const [authMethod, setAuthMethod] = React.useState<AuthState>("sign up");
+	const [email, setEmail] = React.useState("");
+	const [password, setPassword] = React.useState("");
 
 	const handleSwitchAuth = () => {
 		setAuthMethod(() => (authMethod === "sign up" ? "sign in" : "sign up"));
 	};
 
-	const handleSubmit = async (e: any) => {
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		e.console.log("Handle submit");
+
+        if (authMethod === 'sign up') { 
+            const { error: signUpError } = await supabase.auth.signUp({email, password})
+            if (signUpError) console.error('An error has occur when signing up:', signUpError.message)
+                return
+            
+            alert('Please confirm your signing by clicking the link in the email we sent you!')
+        } else { 
+            const { error: signInError } = await supabase.auth.signInWithPassword({email, password})
+            if (signInError) console.error('An error has occur when signing in:', signInError.message)
+                return
+
+            alert('Please confirm your signing by clicking the link in the email we sent you!')
+        }       
+		// console.log("Sending form!", { email: email, password: password });
+		setEmail("");
+		setPassword("");
 	};
 	return (
 		<div className="flex w-96 flex-col justify-center items-center">
@@ -21,10 +40,22 @@ export default function Auth() {
 				</h1>
 
 				<div className="flex flex-col gap-3 mb-8">
-					<input type="text" placeholder="enter your email" />
-					<input type="text" placeholder="enter your password" />
+					<input
+						type="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						placeholder="enter your email"
+					/>
+					<input
+						type="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						placeholder="enter your password"
+					/>
 				</div>
-				<button className="w-full">submit</button>
+				<button type="submit" className="w-full">
+					submit
+				</button>
 				<div className="w-full my-2 h-[1px] bg-white/10"></div>
 				<button onClick={() => handleSwitchAuth()} className="w-full">
 					{authMethod === "sign in"
