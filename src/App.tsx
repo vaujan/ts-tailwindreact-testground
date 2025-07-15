9ba9420a552921b2f5090696e9c6c70f347efb48
vaujan <vaujan@github.com>
2025-07-15 21:55:22 +0700

    simplify setup styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
-import PragmaticDnd from "./components/pragmatic-dnd/pragmatic-dnd";
+// import PragmaticDnd from "./components/pragmatic-dnd/pragmatic-dnd";
+import Kanban from "./components/pragmatic-dnd/kanban";
 
 function App() {
 	return (
-		<div className="flex flex-col gap-6">
-			<PragmaticDnd />
+		<div className="flex min-h-screen bg-stone-700 text-white flex-col gap-6">
+			<div className="md:py-32 md:px-16">
+				{/* <PragmaticDnd /> */}
+
+				<Kanban />
+			</div>
 		</div>
 	);
 }
