b409fa0a6cf93de8120f3e789adbe40a23b94ea7
vaujan <vaujan@github.com>
2025-07-16 20:39:49 +0700

    finished dragging operation

--- src/components/todo/pragmatic-dnd/card.tsx
+++ src/components/todo/pragmatic-dnd/card.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { type Card } from "./types";
+import { type Card as CardType } from "./types";
 import { draggable } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+import { setCustomNativeDragPreview } from "@atlaskit/pragmatic-drag-and-drop/element/set-custom-native-drag-preview";
+import { createPortal } from "react-dom";
 
-export default function Card({ card }: { card: Card }) {
+export default function Card({ card }: { card: CardType }) {
 	const { description, id } = card;
 	const [isDragging, setIsDragging] = React.useState<boolean>(false);
 	const [preview, setPreview] = React.useState<HTMLElement | null>(null);
@@ -24,8 +26,11 @@ export default function Card({ card }: { card: Card }) {
 				setPreview(null);
 			},
 			onGenerateDragPreview({ nativeSetDragImage }) {
-				setCustomNativeDragPreview(nativeSetDragImage, render({ container }) { 
-					setPreview(container)
+				setCustomNativeDragPreview({
+					nativeSetDragImage,
+					render({ container }) {
+						setPreview(container);
+					},
 				});
 			},
 		});
@@ -38,12 +43,28 @@ export default function Card({ card }: { card: Card }) {
 			ref={ref}
 			className={`${
 				isDragging ? "opacity-50" : ""
-			} bg-white/10 flex flex-col gap-2 rounded-md p-3`}
+			} bg-white/10 flex flex-col gap-2 rounded-md p-3 transition-all ease-out cursor-grab `}
 		>
 			<span className="text-sm  rounded-md p-1 bg-orange-900 border border-orange-800 text-orange-200 w-fit">
 				{id}
 			</span>
 			<span>{description}</span>
+			{preview && createPortal(<CardPreview card={card} />, preview)}
+		</div>
+	);
+}
+
+function CardPreview({ card }: { card: CardType }) {
+	const { id, description } = card;
+
+	return (
+		<div className="w-[350px]">
+			<div className="bg-white/10 flex rotate-5 flex-col gap-2 rounded-md p-3 transition-all ease-out cursor-grab">
+				<span className="text-sm  rounded-md p-1 bg-orange-900 border border-orange-800 text-orange-200 w-fit">
+					{id}
+				</span>
+				<span>{description}</span>
+			</div>
 		</div>
 	);
 }
--- src/components/todo/pragmatic-dnd/kanban.tsx
+++ src/components/todo/pragmatic-dnd/kanban.tsx
@@ -1,5 +1,7 @@
+import React from "react";
 import type { Card as CardType } from "./types";
 import Card from "./card";
+import { Plus, Trash } from "lucide-react";
 
 export default function Kanban() {
 	const card: CardType = {
@@ -7,14 +9,30 @@ export default function Kanban() {
 		description: "testing",
 	};
 
+	const [isHovering, setIsHovering] = React.useState<boolean>(false);
+
 	return (
 		<div className="flex flex-col gap-8">
 			<h1 className="text-3xl font-semibold text-gray-200p">My board</h1>
 			{/* Column map */}
 			<div className="flex gap-3">
 				{/* Column */}
-				<div className="w-[350px] p-3 rounded-lg flex flex-col gap-5 bg-black/20">
-					<h3 className="font-medium text-white/50">Todo</h3>
+				<div
+					onMouseEnter={() => setIsHovering(true)}
+					onMouseLeave={() => setIsHovering(false)}
+					className="w-[350px] p-3 rounded-lg flex flex-col gap-5 bg-black/20"
+				>
+					<div className="flex justify-between">
+						<h3 className="font-medium text-white/50">Todo</h3>
+						<div className={`${isHovering ? "" : "invisible"} flex gap-2`}>
+							<button className="size-10 ">
+								<Plus />
+							</button>
+							<button className="size-10">
+								<Trash />
+							</button>
+						</div>
+					</div>
 
 					<Card card={card} />
 				</div>
